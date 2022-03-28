0a01e89c894912daf7eb4bd86b8fcc54e18696c4
parkchangwoo1 <wkwmdsk808@ajou.ac.kr>
2022-03-28 12:41:29 +0900

    router

--- src/shared/App.js
+++ src/shared/App.js
@@ -6,7 +6,7 @@ import '../App.css';
 
 function App() {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<GlobalStyles />
 			<Routes>
 				<Route path="/" element={<Pages.Main />} />
